af1c4f0fdfdfc8f1eb26b2d96ea99f36c366ffd5
Lorenzo <lorenzo.ong@berkeley.edu>
2020-01-29 02:03:58 +0800

    midway through tooltip

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import { Typography, Fab } from "@material-ui/core";
+import { Typography, Fab, Tooltip } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
 import CheckIcon from "@material-ui/icons/Check";
 import BlockIcon from "@material-ui/icons/Block";
 import TrackChangesIcon from "@material-ui/icons/TrackChanges";
+import GraphicEqIcon from "@material-ui/icons/GraphicEq";
 import FingerprintIcon from "@material-ui/icons/Fingerprint";
 import IconButton from "./components/IconButton";
 import ExtendedButton from "./components/ExtendedButton";
+
+const styles = {
+  tooltip: {
+    background: "white"
+  }
+};
 
 class Components extends Component {
   constructor(props) {
@@ -22,19 +30,37 @@ class Components extends Component {
   renderButtons() {}
 
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        <Typography variant="overline" color="textPrimary">
+        <Typography variant="overline" color="textPrimary" style={{ fontSize: "20px", opacity: 0.9 }}>
           Skeuomorphism
         </Typography>
+
         <div className="horizontal-divider" />
+
         <IconButton icon={<CheckIcon />} onClick={this.handleCheck} />
+
         <div className="vertical-divider" />
+
         <IconButton icon={<BlockIcon />} />
         <ExtendedButton icon={<BlockIcon />} label="Verify" />
+
         <div className="horizontal-divider" />
+
         <IconButton icon={<TrackChangesIcon />} />
         <IconButton icon={<FingerprintIcon />} />
+        <div className="vertical-divider" />
+        <Tooltip PopperProps={{ className: classes.tooltip }} title={<div>Tooltip text goes here</div>}>
+          <div style={{ display: "inline-block" }}>
+            <ExtendedButton icon={<GraphicEqIcon />} label="Suggest" />
+          </div>
+        </Tooltip>
+        {/* <Tooltip title="text">
+          <div style={{ display: "inline-block" }}>
+            <ExtendedButton icon={<GraphicEqIcon />} label="Suggest" />
+          </div>
+        </Tooltip> */}
         {/* <Fab variant="extended" size="small">
           <CheckIcon />
           Verify
@@ -45,4 +71,4 @@ class Components extends Component {
   }
 }
 
-export default Components;
+export default withStyles(styles)(Components);
